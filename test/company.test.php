8f9a48f306feed3f75a3fe448c07cbe1fe268ab7
Adrian Rollett <adrian@clikfocus.com>
2011-08-31 10:28:55 -0600

    added basic support for companies

---
+++ test/company.test.php
@@ -0,0 +1,34 @@
+<?php
+require_once("../lib/HighriseAPI.class.php");
+
+if (count($argv) != 3)
+	die("Usage: php company.test.php [account-name] [access-token]\n");
+
+
+$highrise = new HighriseAPI();
+$highrise->debug = false;
+$highrise->setAccount($argv[1]);
+$highrise->setToken($argv[2]);
+
+$companies = $highrise->findAllCompanies();
+foreach ($companies as $c) {
+  print_r ($c);
+}
+
+$company = new HighriseCompany($highrise);
+$company->setName("Test Company");
+$company->setBackground("API Test");
+$company->setVisibleTo("Owner");
+$company->addEmailAddress("test@example.com");
+$company->addPhoneNumber("8008888888");
+$company->save();
+print "Company ID is: " . $company->getId() . "\n";
+$company->addEmailAddress("test2@example.com");
+$company->save();
+print "Company ID after save is: " . $company->getId() . "\n";
+print_r($company);
+
+$companies = $highrise->findCompaniesBySearchTerm("Test Company");
+print_r($companies);
+
+$company->delete();
